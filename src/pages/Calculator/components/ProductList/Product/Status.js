2675d0cd354bb2e4fa8e6419e7e9c48bd7d567ea
Андрей Прокофьев <65781959+kensoi@users.noreply.github.com>
2023-08-17 22:14:24 +0500

    ProductList was removed

--- src/pages/Calculator/components/ProductList/Product/Status.js
+++
@@ -1,8 +0,0 @@
-export function StatusRemoveWarn () {
-    return <div className="add-product-status">
-        Продукт был удалён
-    </div>
-}
-export function StatusProductRemoved () {
-    return <></>
-}
