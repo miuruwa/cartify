9488b1dd2500133d47f3ed5c4ca26d8faad727c5
Андрей Прокофьев <65781959+kensoi@users.noreply.github.com>
2023-08-22 06:06:39 +0500

    theme refactor

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import useAppPartitions from "utils/useAppPartitions"
 import Header from "Header"
 import Calculator from "Calculator"
 
+import "schema/stylesheet.scss"
+
 export default function App () {
     useAppPartitions()
 
