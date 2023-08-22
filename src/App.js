8a65bea8e360b9aa77257075f1d7ef635a50dec0
Андрей Прокофьев <65781959+kensoi@users.noreply.github.com>
2023-08-22 06:11:21 +0500

    setpartition error patch

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import {
     ToolKitContext
 } from "@webx/toolkit"
 
-import useAppPartitions from "utils/useAppPartitions"
-
 import Header from "Header"
 import Calculator from "Calculator"
 
 import "theme/stylesheet.scss"
 
 export default function App () {
-    useAppPartitions()
-
     return <ToolKitContext>
         <Header/>
         <Calculator/>
--- src/Calculator/index.js
+++ src/Calculator/index.js
@@ -4,12 +4,15 @@ import {
     useToolKit
 } from "@webx/toolkit"
 
+import useAppPartitions from "utils/useAppPartitions"
+
 import {
     DesktopTemplate, MobileTemplate, TabletTemplate
 } from "./templates"
 
 export default function Calculator () {
     const toolkit = useToolKit()
+    useAppPartitions()
 
     if (toolkit.settings.windowWidth >= 1280) {
         return <DesktopTemplate />
