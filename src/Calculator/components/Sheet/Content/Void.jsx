5a4a538557067f3b8eed37a858fd04a70f37e057
Miuruwa <65781959+miuruwa@users.noreply.github.com>
2023-11-06 20:54:05 +0500

    no product language support

--- src/Calculator/components/Sheet/Content/Void.jsx
+++ src/Calculator/components/Sheet/Content/Void.jsx
@@ -1,7 +1,14 @@
+import { useToolKit } from "@webx/toolkit"
+import languages from "./languages"
+
+
 function Void() {
+    const toolkit = useToolKit()
+    const actualLanguage = languages[toolkit.settings.language]
+
     return <div className="sheet-void">
         <p>
-            <nobr> Добавьте товар </nobr> <nobr> при помощи </nobr> панели <nobr> "Добавить продукт" </nobr>
+            <nobr> {actualLanguage[0]} </nobr> <nobr> {actualLanguage[1]} </nobr> {actualLanguage[2]} <nobr> "{actualLanguage[3]}" </nobr>
         </p>
     </div>
 }
