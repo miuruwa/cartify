1403c444637b786f6f37347feee074e27d1f682c
Miuruwa <65781959+miuruwa@users.noreply.github.com>
2023-11-06 11:54:40 +0500

    Header Home language support

--- src/Header/components/Home/index.jsx
+++ src/Header/components/Home/index.jsx
@@ -1,11 +1,15 @@
 import Icon from "@webx/icons/HeaderLogoIcon"
 import "./stylesheet.scss"
 
+import languages from "./languages"
+import { useToolKit } from "@webx/toolkit"
+
 export default function ({Mobile}) {
+    const toolkit = useToolKit()
     const divProps = {
         children: <Icon />,
         className: Mobile ? "header-home mobile" : "header-home",
-        title: "На главную",
+        title: languages[toolkit.settings.language],
         onClick: () => {
             window.location.pathname = "/home"
         }
