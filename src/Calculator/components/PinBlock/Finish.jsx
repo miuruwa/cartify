2b0c880056f3cbcaa1865c7c1838dd7bd6598140
Miuruwa <65781959+miuruwa@users.noreply.github.com>
2023-11-06 21:13:40 +0500

    Update Finish.jsx

--- src/Calculator/components/PinBlock/Finish.jsx
+++ src/Calculator/components/PinBlock/Finish.jsx
@@ -4,7 +4,7 @@ import languages from "./languages"
 
 export default function () {
     const toolkit = useToolKit()
-    const actualLanguage = language[toolkit.settings.language]
+    const actualLanguage = languages[toolkit.settings.language]
 
     return <div className="add-product-status finish">
         {actualLanguage.done}
